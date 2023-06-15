7ca9ceeb2323868be2e2fa04f9d1dab0279d1e98
Kjell-Morten <post@kjellmorten.no>
2023-06-15 14:01:56 +0200

    Ignore redis data when watching tests

--- ava.config.cjs
+++ ava.config.cjs
@@ -1,6 +1,6 @@
 module.exports = {
   extensions: { ts: 'module' },
   nodeArguments: ['--loader=ts-node/esm', '--no-warnings'],
-  ignoredByWatcher: ['{coverage,dist,media}/**', '**/*.md'],
+  ignoredByWatcher: ['{coverage,dist,media,redisdata}/**', '**/*.md'],
   files: ['src/**/*.test.ts'],
 }
