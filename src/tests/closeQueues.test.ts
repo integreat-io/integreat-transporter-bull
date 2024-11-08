d6885c5cce8dcb262451fee0c458cc765eca2da5
Kjell-Morten <post@kjellmorten.no>
2024-11-08 18:20:52 +0100

    Fix bug where closed sub-queue would stop others from connecting to that sub-queue

--- src/tests/closeQueues.test.ts
+++ src/tests/closeQueues.test.ts
@@ -1,6 +1,7 @@
 import test from 'node:test'
 import assert from 'node:assert/strict'
-import { Connection } from '../types.js'
+import wait from './helpers/wait.js'
+import type { Connection } from '../types.js'
 
 import transporter from '../index.js'
 
@@ -82,8 +83,127 @@ test('should not close queue for other connections', async (t) => {
   )
 })
 
+test('should not close queue for other connections when first connection has been used', async (t) => {
+  const queueId0 = 'ns63'
+  const queueId1 = 'ns64'
+  const options0 = {
+    queueId: queueId0,
+    redis: { host: 'localhost', port: 6379 },
+  }
+  const options1 = {
+    queueId: queueId1,
+    redis: { host: 'localhost', port: 6379 },
+  }
+  const action = {
+    type: 'SET',
+    payload: { type: 'entry', data: { id: 'ent1', $type: 'entry' } },
+    meta: { ident: { id: 'johnf' } },
+  }
+
+  const conn0 = await transporter.connect(options0, null, null, emit)
+  const sendResponse0 = await transporter.send(action, conn0) // We're sending while the queue is still open
+  const conn1 = await transporter.connect(options1, null, null, emit)
+  t.after(async () => {
+    // Close second connection when we're done
+    await transporter.disconnect(conn1)
+  })
+  await transporter.disconnect(conn0)
+  const sendResponse1 = await transporter.send(action, conn1) // This queue is not closed, so we should get ok
+
+  assert(conn0, 'No connection')
+  assert.equal(
+    sendResponse0.status,
+    'ok',
+    `First send() responded with ${sendResponse0.status}`,
+  )
+  assert.equal(
+    sendResponse1.status,
+    'ok',
+    `Second send() responded with ${sendResponse1.status}`,
+  )
+})
+
+test('should reconnect when queue has been closed by other connection', async (t) => {
+  const queueId0 = 'ns65'
+  const queueId1 = 'ns66'
+  const options0 = {
+    queueId: queueId0,
+    redis: { host: 'localhost', port: 6379 },
+  }
+  const options1 = {
+    queueId: queueId1,
+    redis: { host: 'localhost', port: 6379 },
+  }
+  const action = {
+    type: 'SET',
+    payload: { type: 'entry', data: { id: 'ent1', $type: 'entry' } },
+    meta: { ident: { id: 'johnf' } },
+  }
+
+  const conn0 = await transporter.connect(options0, null, null, emit)
+  const sendResponse0 = await transporter.send(action, conn0) // We're sending while the queue is still open
+  await transporter.disconnect(conn0)
+  const conn1 = await transporter.connect(options1, null, null, emit)
+  t.after(async () => {
+    // Close second connection when we're done
+    await transporter.disconnect(conn1)
+  })
+  const sendResponse1 = await transporter.send(action, conn1) // This queue is not closed, so we should get ok
+
+  assert(conn0, 'No connection')
+  assert.equal(
+    sendResponse0.status,
+    'ok',
+    `First send() responded with ${sendResponse0.status}`,
+  )
+  assert.equal(
+    sendResponse1.status,
+    'ok',
+    `Second send() responded with ${sendResponse1.status}`,
+  )
+})
+
+test('should reconnect to sub queue previously closed by other connectin', async (t) => {
+  const queueId = 'ns67'
+  const options0 = {
+    queueId,
+    redis: { host: 'localhost', port: 6379 },
+  }
+  const options1 = {
+    queueId,
+    redis: { host: 'localhost', port: 6379 },
+  }
+  const action = {
+    type: 'SET',
+    payload: { type: 'entry', data: { id: 'ent1', $type: 'entry' } },
+    meta: { ident: { id: 'johnf' } },
+  }
+
+  const conn0 = await transporter.connect(options0, null, null, emit)
+  const sendResponse0 = await transporter.send(action, conn0) // We're sending while the queue is still open
+  await transporter.disconnect(conn0)
+  const conn1 = await transporter.connect(options1, null, null, emit)
+  t.after(async () => {
+    // Close second connection when we're done
+    await transporter.disconnect(conn1)
+  })
+  const sendResponse1 = await transporter.send(action, conn1) // This queue was closed, but should be reconnected
+
+  assert(conn0, 'No connection')
+  assert.equal(
+    sendResponse0.status,
+    'ok',
+    `First send() responded with ${sendResponse0.status} ${sendResponse0.error}`,
+  )
+  assert.equal(
+    sendResponse1.status,
+    'ok',
+    `Second send() responded with ${sendResponse1.status} ${sendResponse1.error}`,
+  )
+})
+
 test('should not close sub queue for other connections', async (t) => {
-  const queueId = 'ns63'
+  const queueId = 'ns68'
   const options0 = {
     queueId,
     subQueueId: 'sub0',
